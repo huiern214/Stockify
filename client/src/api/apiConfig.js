c7130d50b60f2b93f557dc6578e563084452a31c
NKHon831 <khaihon030831@gmail.com>
2024-03-24 01:24:42 +0800

    fix chart

--- client/src/api/apiConfig.js
+++ client/src/api/apiConfig.js
@@ -1,4 +1,4 @@
-const API_KEY = 'zzIsIQczVCtNjvqK3QSNXkzW0T4fg3XB';
+const API_KEY = 'sPjSlyqmXn0XzSgR7PZ0NjtROnwrwh8B';
 // const API_KEY = 'sPjSlyqmXn0XzSgR7PZ0NjtROnwrwh8B';
 // const API_KEY = 'b378ShUTaESYwt0piNnjscec4SaMxqxe';
 
